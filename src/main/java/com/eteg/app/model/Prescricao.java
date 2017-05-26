fbecf3351b234c95ae639f0eb36ebe970e79de09
Alaor <alaor.resende@gmail.com>
2017-05-26 16:14:18 -0300

    Mapeamento das entidades.

---
+++ src/main/java/com/eteg/app/model/Prescricao.java
@@ -0,0 +1,34 @@
+package com.eteg.app.model;
+
+import org.hibernate.annotations.*;
+import org.hibernate.annotations.CascadeType;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by alaor on 26/05/17.
+ */
+@Entity
+@Table(name="prescricao")
+public class Prescricao {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "data")
+    private Date data;
+
+    @Column(name = "id_paciente")
+    private Paciente paciente;
+
+    @OneToMany(mappedBy = "medicamento", orphanRemoval = true)
+    @NotFound(action = NotFoundAction.IGNORE)
+    @Cascade({CascadeType.ALL})
+    private List<PrescricaoMedicamento> medicamentos;
+}
