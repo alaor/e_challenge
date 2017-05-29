15c7318a1c85dd318ccec42d451790e110f2c872
Alaor <alaor.resende@gmail.com>
2017-05-29 15:41:40 -0300

    Criacao de DAOs de Prescricao e PrescricaoMedicamento.

---
+++ src/main/java/com/eteg/app/dao/PrescricaoDao.java
@@ -0,0 +1,10 @@
+package com.eteg.app.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by alaor on 29/05/17.
+ */
+public interface PrescricaoDao extends JpaRepository<PrescricaoDao, Long> {
+
+}
