cee249dc0ececa2fcb99564c86943fcb621c3f4a
Alaor <alaor.resende@gmail.com>
2017-05-29 15:38:37 -0300

    Alterando tipo do Id de Prescricao e PrescricaoMedicamento de Integer para Long.

--- src/main/java/com/eteg/app/model/PrescricaoMedicamento.java
+++ src/main/java/com/eteg/app/model/PrescricaoMedicamento.java
@@ -12,7 +12,7 @@ public class PrescricaoMedicamento {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(name = "frequencia")
     private Integer frequencia;
@@ -26,11 +26,11 @@ public class PrescricaoMedicamento {
     })
     private Medicamento medicamento;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
